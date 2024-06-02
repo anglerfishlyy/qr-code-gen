38161472a664b8485c911d43955c43fa0f4e7552
anglerfishlyy <kavyasamudralaA7@outlook.com>
2024-06-02 14:47:42 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// App.js
+import React from 'react';
+import QRCodeGenerator from './index.jsx'; // Corrected import
+
+function App() {
+  return (
+    <div className="App">
+      <QRCodeGenerator /> {/* Corrected rendering */}
+    </div>
+  );
+}
+
+export default App;
